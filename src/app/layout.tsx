2a2898364f54c021b12ae9959d8e14a8af76c227
pickmymaid <pickmymaid@gmail.com>
2026-08-25 20:45:04 +0530

    asdf

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Manrope } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import { Navbar } from "@/components/layout/Navbar";
 import { Footer } from "@/components/layout/Footer";
@@ -62,27 +61,6 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://api.pickmymaid.com" />
       </head>
       <body className="min-h-full flex flex-col">
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '4436313626504462');
-fbq('track', 'PageView');`}
-        </Script>
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=4436313626504462&ev=PageView&noscript=1"
-            alt=""
-          />
-        </noscript>
         {/* <NoRightClick /> */}
         <Navbar />
         <main className="flex-1">{children}</main>
